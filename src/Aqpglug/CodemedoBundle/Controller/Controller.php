8fd17191f2d2c3b48f20cad54ec9765e69045a5c
Jose Vargas Paz <ejosvp@gmail.com>
2011-06-17 17:51:22 -0500

    agregado getconfig a controller

--- src/Aqpglug/CodemedoBundle/Controller/Controller.php
+++ src/Aqpglug/CodemedoBundle/Controller/Controller.php
@@ -4,14 +4,24 @@ namespace Aqpglug\CodemedoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as Base_Controller;
 use Aqpglug\CodemedoBundle\Repository\BlockRepository;
+use Aqpglug\CodemedoBundle\Extension\Config;
 
 class Controller extends Base_Controller
 {
+
     /**
      * @return BlockRepository
      */
     public function getRepo()
     {
         return $this->getDoctrine()->getRepository('Aqpglug\CodemedoBundle\Entity\Block');
+    }
+
+    /**
+     * @return Config
+     */
+    public function getConfig()
+    {
+        return $this->get('codemedo');
     }
 }
