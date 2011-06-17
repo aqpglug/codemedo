c5a07997a7c58a27c9f05c966bfa6ab36f10a068
Carlos Zúñiga <carlos.zun@gmail.com>
2011-06-17 01:22:41 -0500

    Cambio de campo de texto a combo en el formulario

--- src/Aqpglug/CodemedoBundle/Form/BlockType.php
+++ src/Aqpglug/CodemedoBundle/Form/BlockType.php
@@ -6,10 +6,20 @@ use Symfony\Component\Form\FormBuilder;
 
 class BlockType extends AbstractType
 {
+    // TODO: traer esto desde el config
+    public $types = array(
+        'page' => 'Página',
+        'article' => 'Artículo',
+        'project' => 'Proyecto',
+        'event' => 'Evento',
+        'member' => 'Miembro',
+    );
+
     public function buildForm(FormBuilder $builder, array $options)
     {
-        $builder->add('type')
+        $builder->add('type', 'choice', array('choices' => $this->types))
                 ->add('title')
+                ->add('slug')
                 ->add('content');
     }
 }
