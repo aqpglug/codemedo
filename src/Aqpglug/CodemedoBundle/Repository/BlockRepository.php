b3eedbaf0350f1dfe5f85f69a547a48f4e29849e
Jose Vargas Paz <ejosvp@gmail.com>
2011-06-16 20:35:55 -0500

    modificado repo

--- src/Aqpglug/CodemedoBundle/Repository/BlockRepository.php
+++ src/Aqpglug/CodemedoBundle/Repository/BlockRepository.php
@@ -13,13 +13,35 @@ class BlockRepository extends EntityRepository
     {
         return $this->findBy(array(
             'type' => 'page',
-            'id' => $id ));
+            'id' => $id));
     }
-    
+
     public function findPageBySlug($slug)
     {
         return $this->findBy(array(
             'type' => 'page',
-            'slug' => $slug ));
+            'slug' => $slug));
+    }
+
+    public function findAllSortedBy($type, $field, $nb = null)
+    {
+        $query = $this->queryAllSortedBy($field);
+
+        if (null !== $nb) {
+            $query->setMaxResults($nb);
+        }
+
+        return $query->execute();
+    }
+
+    public function queryAllSortedBy($type, $field)
+    {
+        $qb = $this->createQueryBuilder('e');
+        $qb->where('e.type = ?', $type);
+        $qb->andWhere('e.published = ?', true);
+        $qb->orderBy('e.' . $field, 'title' === $field ? 'asc' : 'desc');
+        $query = $qb->getQuery();
+
+        return $query;
     }
 }
