9e2a9006c78bdf27369c3560f592eec95a8a97b8
Jose Vargas Paz <ejosvp@gmail.com>
2011-06-19 23:56:30 -0500

    agregado mapa personalizado para eventos

---
+++ src/Aqpglug/CodemedoBundle/Resources/public/js/event_maps.js
@@ -0,0 +1,14 @@
+function initialize(place, lat, lng, zoom) {
+    var _zoom = (zoom != null) ? zoom: 15;
+    $(place).gmap3(
+    {
+        action: 'init',
+        options:{
+            center:[lat, lng],
+            zoom: _zoom
+        }
+    },{
+        action:'addMarker',
+        latLng:[lat, lng]
+    });
+}
