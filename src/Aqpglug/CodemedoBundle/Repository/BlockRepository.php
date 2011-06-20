d1635cb5c0f10931e18a3b2998d193f50d4a1e6e
Jos Vargas Paz <ejosvp@gmail.com>
2011-06-19 19:24:00 -0500

    eliminado metodo gethome del repo

--- src/Aqpglug/CodemedoBundle/Repository/BlockRepository.php
+++ src/Aqpglug/CodemedoBundle/Repository/BlockRepository.php
@@ -9,13 +9,6 @@ use Aqpglug\CodemedoBundle\Entity\Block;
 class BlockRepository extends EntityRepository
 {
 
-    public function getHome($id)
-    {
-        return $this->findOneBy(array(
-            'type' => 'page',
-            'id' => $id));
-    }
-
     public function findPageBySlug($slug)
     {
         return $this->findOneBy(array(
