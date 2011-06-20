72438a8e3a9994aaaf9361e0cfd861acbfaf7b8c
Jos Vargas Paz <ejosvp@gmail.com>
2011-06-19 20:54:56 -0500

    mejorada clase config

--- src/Aqpglug/CodemedoBundle/Extension/Config.php
+++ src/Aqpglug/CodemedoBundle/Extension/Config.php
@@ -6,29 +6,39 @@ use Symfony\Component\Yaml\Yaml;
 
 class Config
 {
-
     protected $data;
-
     public function __construct()
     {
+        $base_config = array_values(array('homepage', 'types'));
+        $base_types = array('page' => array('label' => 'Página'));
+
         $this->data = Yaml::parse(__DIR__ . "/../Resources/config/codemedo.yml");
+
         $keys = array_keys($this->data);
 
-        if (count(array_diff($keys, array('homepage', 'types'))))
-            throw new \InvalidArgumentException("configuración no valida, debe tener 'homepage' y 'types'");
+        if (array_values(array_intersect($keys, $base_config)) !== $base_config)
+            throw new \InvalidArgumentException();
 
-        foreach ($this->data['types'] as $key => $value) {
-            if (!array_key_exists('label', $value)) {
-                throw new \InvalidArgumentException("configuración no valida, los 'types' necesitan un campo 'label'");
+        $this->data['types'] = array_merge($base_types, $this->data['types'] ? : array());
+
+        foreach ($this->data['types'] as $value) {
+            if (!array_key_exists('label', $value ? : array())) {
+                throw new \InvalidArgumentException();
             }
         }
     }
-
+    public function get($config)
+    {
+        return $this->data[$config] ? : array();
+    }
+    public function getHome()
+    {
+        return $this->get('homepage');
+    }
     public function getTypes()
     {
-        return $this->data['types'];
+        return $this->get('types');
     }
-
     public function getLabels()
     {
         $labels = array();
@@ -37,25 +47,18 @@ class Config
         }
         return $labels;
     }
-
     public function getMeta($type = null)
     {
-        if(in_array($type, $this->getTypes()))
+        if (in_array($type, $this->getTypes()))
             throw new \InvalidArgumentException();
-        
+
         $meta = $this->data['types'];
         foreach ($meta as $key => $value) {
             $meta[$key] = array_key_exists('metadata', $value) ? $value['metadata'] : array();
         }
-        
-        if($type !== null)
+
+        if ($type !== null)
             return $meta[$type];
         return $meta;
     }
-
-    public function getHome()
-    {
-        return $this->data['homepage'];
-    }
-
 }
