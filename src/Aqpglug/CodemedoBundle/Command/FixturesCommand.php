af09699fd0285783d1b57057cd90ca2dfbc23e96
Jose Vargas Paz <ejosvp@gmail.com>
2011-06-27 12:34:50 -0500

    terminado fixtures

--- src/Aqpglug/CodemedoBundle/Command/FixturesCommand.php
+++ src/Aqpglug/CodemedoBundle/Command/FixturesCommand.php
@@ -12,13 +12,14 @@ use Aqpglug\CodemedoBundle\Entity\Block;
 
 class FixturesCommand extends ContainerAwareCommand
 {
+
     protected function configure()
     {
         $this
                 ->setName('codemedo:fixtures')
                 ->setDescription('agrega data desde un YML en la a la base de datos')
                 ->setDefinition(array(
-                    new InputOption('path', null, InputOption::VALUE_OPTIONAL, '/../Resources/fixtures/Block.yml'),
+                    new InputOption('path', null, InputOption::VALUE_OPTIONAL, ''),
                     new InputOption('clean', null, InputOption::VALUE_OPTIONAL, false),
                 ))
                 ->setHelp(<<<EOT
@@ -35,28 +36,39 @@ puede agregar la opcion --clean para limpiar la base de datos antes de insertar 
 EOT
         );
     }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $path = __DIR__ . $input->getOption('path');
-        $clean = __DIR__ . $input->getOption('clean');
-        $data = Yaml::parse($path);
+        $path = sprintf("%s/../%s", __DIR__, ($input->getOption('path') ? : "Resources/fixtures/Block.yml"));
+        $clean = $input->getOption('clean');
         
+        $output->writeln(sprintf("<info>Leyendo </info><comment>%s</comment>", realpath($path)));
+        $data = Yaml::parse($path);
+
         /* @var $em \Doctrine\ORM\EntityManager */
         $em = $this->getContainer()->get('doctrine')->getEntityManager();
-        
+
         if($clean)
-            $em->createQuery('DELETE a FROM Aqpglug\CodemedoBundle\Entity\Block a');
-        
-        foreach ($data as $item) {
-            $block = new Block();
-            $block->setType($item['type']);
-            $block->setTitle($item['title']);
-            $block->setContent($item['content']);
-            $block->setPublished($item['published']);
-            $block->setFeatured($item['featured']);
-            $block->setImage($item['image']);
-            $block->setMetadata($item['metadata']);
-            $em->persist($block);
+        {
+            $output->writeln("<info>Eliminando registros de </info><comment>Block</comment>");
+            $em->createQuery('DELETE Aqpglug\CodemedoBundle\Entity\Block a')->execute();
+        }
+
+        foreach($data as $section => $items)
+        {
+            $output->writeln(sprintf("<info>Agregando </info><comment>%s</comment>", $section));
+            foreach($items as $item)
+            {
+                $block = new Block();
+                $block->setType($item['type']);
+                $block->setTitle($item['title']);
+                $block->setContent($item['content']);
+                $block->setPublished($item['published']);
+                $block->setFeatured($item['featured']);
+                $block->setImage($item['image']);
+                $block->setMetadata($item['metadata']);
+                $em->persist($block);
+            }
         }
         $em->flush();
     }
