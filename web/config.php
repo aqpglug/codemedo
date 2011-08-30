047384123ad7e849a25f6d42077237c80de18818
Carlos Zúñiga <carlos.zun@gmail.com>
2011-08-30 16:52:05 -0500

    Cambios para Symfony v2.0.1

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!isset($_SERVER['HTTP_HOST'])) {
-    die('This script cannot be run from the CLI. Run it from a browser.');
+    exit('This script cannot be run from the CLI. Run it from a browser.');
 }
 
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
@@ -9,7 +9,7 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '::1',
 ))) {
     header('HTTP/1.0 403 Forbidden');
-    die('This script is only accessible from localhost.');
+    exit('This script is only accessible from localhost.');
 }
 
 $majorProblems = array();
@@ -47,6 +47,10 @@ if (!defined('LIBXML_COMPACT')) {
 
 if (!((function_exists('apc_store') && ini_get('apc.enabled')) || function_exists('eaccelerator_put') && ini_get('eaccelerator.enable') || function_exists('xcache_set'))) {
     $minorProblems[] = 'Install and enable a <strong>PHP accelerator</strong> like APC (highly recommended).';
+}
+
+if (!(!(function_exists('apc_store') && ini_get('apc.enabled')) || version_compare(phpversion('apc'), '3.0.17', '>='))) {
+    $majorProblems[] = 'Upgrade your <strong>APC</strong> extension (3.0.17+)';
 }
 
 if (!function_exists('token_get_all')) {
@@ -87,7 +91,7 @@ if (!class_exists('Locale')) {
         $version = $matches[1];
     }
 
-    if(!version_compare($matches[1], '4.0', '>=')) {
+    if (!version_compare($version, '4.0', '>=')) {
         $minorProblems[] = 'Upgrade your <strong>intl</strong> extension with a newer ICU version (4+).';
     }
 }
@@ -98,6 +102,14 @@ if (!class_exists('SQLite3') && !in_array('sqlite', PDO::getAvailableDrivers()))
 
 if (!function_exists('json_encode')) {
     $majorProblems[] = 'Install and enable the <strong>json</strong> extension.';
+}
+
+if (!function_exists('session_start')) {
+    $majorProblems[] = 'Install and enable the <strong>session</strong> extension.';
+}
+
+if (!function_exists('ctype_alpha')) {
+    $majorProblems[] = 'Install and enable the <strong>ctype</strong> extension.';
 }
 
 // php.ini
